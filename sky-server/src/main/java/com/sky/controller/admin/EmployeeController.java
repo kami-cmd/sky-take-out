b99a3c7c08c4c6b0fe9d6926f1d526b7dd9d3d1a
kami-cmd <2839545149@qq.com>
2025-12-31 15:57:40 +0800

    新增员工开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -8,6 +9,8 @@ import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +27,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/admin/employee")
 @Slf4j
+@Api(tags = "员工相关操作")
 public class EmployeeController {
 
     @Autowired
@@ -37,6 +41,7 @@ public class EmployeeController {
      * @param employeeLoginDTO
      * @return
      */
+    @ApiOperation("员工登录操作")
     @PostMapping("/login")
     public Result<EmployeeLoginVO> login(@RequestBody EmployeeLoginDTO employeeLoginDTO) {
         log.info("员工登录：{}", employeeLoginDTO);
@@ -58,6 +63,7 @@ public class EmployeeController {
                 .token(token)
                 .build();
 
+
         return Result.success(employeeLoginVO);
     }
 
@@ -66,9 +72,23 @@ public class EmployeeController {
      *
      * @return
      */
+    @ApiOperation("员工退出操作")
     @PostMapping("/logout")
     public Result<String> logout() {
         return Result.success();
     }
 
+    /**
+     *
+     * @param employeeDTO
+     * @return
+     */
+
+    @ApiOperation("员工注册操作")
+    @PostMapping
+    public Result save(@RequestBody EmployeeDTO employeeDTO) {
+        log.info("新增员工:{}", employeeDTO);
+        employeeService.save(employeeDTO);
+        return Result.success();
+    }
 }
